b806195fbbd3c83f75e2c37f3cd076cba43a772d
zkxshg <35519607+zkxshg@users.noreply.github.com>
2020-02-11 16:21:34 +0800

    Update 9_Palindrome_Number.cpp

--- 9_Palindrome_Number.cpp
+++ 9_Palindrome_Number.cpp
@@ -21,3 +21,37 @@ public:
         return pali;
     }
 };
+
+//  Demo:
+#include <cmath>
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    int x;
+    cin >> x;
+    if (x < 0) return false;
+    int len = int(log10(x));
+    cout << len << endl;
+    bool pali = true;
+    int n = 0;
+    int m;
+    while (n < len / 2 + 1)
+    {
+
+        m = len - n;
+        int x_n = x / int(pow(10, n)) % 10;
+        int x_m = x / int(pow(10, m)) % 10;
+        cout << x_n << endl;
+        cout << x_m <<endl;
+        if (x_n != x_m)
+        {
+            pali = false;
+            break;
+        }
+        n++;
+    }
+        cout << "pali: " << pali << endl;
+        return 0;
+}
