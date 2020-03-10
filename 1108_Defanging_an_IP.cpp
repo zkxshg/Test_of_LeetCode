f7b639b5ea4524f9a5fc046a1de53451ff188cda
zkxshg <35519607+zkxshg@users.noreply.github.com>
2020-03-10 15:54:04 +0800

    Update 1108_Defanging_an_IP.cpp

--- 1108_Defanging_an_IP.cpp
+++ 1108_Defanging_an_IP.cpp
@@ -4,9 +4,11 @@ class Solution {
 public:
     string defangIPaddr(string line) {
         string::size_type pos = line.find('.');
-	      line=line.replace(pos,1,"[.]");
-	      string::size_type pos2 = line.find('.', pos+2);
+	line=line.replace(pos,1,"[.]");
+	    
+	string::size_type pos2 = line.find('.', pos+2);
         line=line.replace(pos2,1,"[.]");
+	    
         string::size_type pos3 = line.find('.', pos2+2);
         line=line.replace(pos3,1,"[.]");
         return line;
