a18a15d2bb354abeacea8da17d818f28aab722d8
zkxshg <35519607+zkxshg@users.noreply.github.com>
2020-03-11 01:48:43 +0800

    Create 50_Pow(x_n).cpp

---
+++ 50_Pow(x_n).cpp
@@ -0,0 +1,11 @@
+#include <cmath>
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if (x > 0) return exp(n*log(x));
+        else if (x == 0) return 0;
+        else if ((n % 2) == 0) return exp(n*log(-x));
+        else return -exp(n*log(-x));
+        return exp(n*log(x));
+    }
+};
