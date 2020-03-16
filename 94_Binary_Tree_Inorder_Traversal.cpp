0d490ff30efdf8875059584a65ea4794290a8853
zkxshg <35519607+zkxshg@users.noreply.github.com>
2020-03-17 02:14:22 +0800

    Create 94_Binary_Tree_Inorder_Traversal.cpp

---
+++ 94_Binary_Tree_Inorder_Traversal.cpp
@@ -0,0 +1,27 @@
+// same to 145. Binary Tree Postorder Traversal
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+ 
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> result;
+        if(!root) return result;
+        recSearch(root, result);
+        return result;
+    }
+    void recSearch(TreeNode* root, vector<int> & result){
+        if (root){
+            if (root->left) recSearch(root->left, result);
+            result.push_back(root->val);
+            if (root->right) recSearch(root->right, result);
+        }
+    }
+};
