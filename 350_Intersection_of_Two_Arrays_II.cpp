57e35f01ce982d27bd85b77cde061f0e38ed1a0c
zkxshg <35519607+zkxshg@users.noreply.github.com>
2020-03-29 01:35:44 +0800

    Create 350_Intersection_of_Two_Arrays_II.cpp

---
+++ 350_Intersection_of_Two_Arrays_II.cpp
@@ -0,0 +1,18 @@
+// for + delete
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> result;
+        if (nums1.size() == 0 || nums2.size() == 0) return result;
+        for (int i = 0; i < nums1.size(); i++){
+            for (int j = 0; j < nums2.size(); j++){
+                if (nums1[i] == nums2[j]) {
+                    result.push_back(nums1[i]);
+                    nums2.erase(nums2.begin()+j);
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+};
