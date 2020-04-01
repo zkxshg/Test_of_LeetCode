a12ebd4522c811d981d4bdd92e913a78ef819a9a
zkxshg <35519607+zkxshg@users.noreply.github.com>
2020-04-02 04:00:41 +0800

    Create 1342_Number_of_Steps_to_Reduce_a_Number_to_Zero.cpp

---
+++ 1342_Number_of_Steps_to_Reduce_a_Number_to_Zero.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int numberOfSteps (int num) {
+        int couter = 0;
+        while(num != 0){
+            if (num % 2 == 1) num -= 1;
+            else num /= 2;
+            couter++;
+        }
+        return couter;
+    }
+};
