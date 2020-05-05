a5662085c7d5c962aabbb6a3512ed9fd58e9c4f4
zkxshg <35519607+zkxshg@users.noreply.github.com>
2020-05-05 13:31:22 +0800

    Create 1009_Complement_of_Base_10_Integer.cpp

---
+++ 1009_Complement_of_Base_10_Integer.cpp
@@ -0,0 +1,14 @@
+// bit mani
+class Solution {
+public:
+    int bitwiseComplement(int N) {
+        if (N == 0 || N == 1) return 1 - N;
+        int spos = 31;
+        while(spos > 0) {
+            if(N & (1 << spos)) break;
+            spos--;
+        }
+        for (int i = 0; i < spos + 1; i++) N ^= (1 << i);
+        return N;
+    }
+};
