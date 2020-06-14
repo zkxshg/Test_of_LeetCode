487ca33195d8097bd5f7a50b87a32cc6fef447f1
zkxshg <35519607+zkxshg@users.noreply.github.com>
2020-06-15 03:04:07 +0800

    Update 787_Cheapest_Flights_Within_K_Stops.cpp

--- 787_Cheapest_Flights_Within_K_Stops.cpp
+++ 787_Cheapest_Flights_Within_K_Stops.cpp
@@ -1,11 +1,10 @@
-
-// Dijkstra算法
+// Dijkstra算法 / BFS
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int K) {
         if (src == dst) return 0;
-        
         if (flights.size() == 0) return -1;
+        int kk = K;
         
         vector<vector<int>> path(n, vector<int>(n, n * 10001));
         for (int i = 0; i < flights.size(); i++) path[flights[i][0]][flights[i][1]] = flights[i][2];
@@ -14,36 +13,24 @@ public:
         for (int i = 0; i < src; i++) dsts.push_back(i);
         for (int i = src + 1; i < n; i++) dsts.push_back(i);
         
-        while (K > 1) {
-            int min_dst = -1, min_path = n * 10001;
-            for (int i = 0; i < dsts.size(); i++) {
-                if (path[src][dsts[i]] < min_path) {
-                    min_path = path[src][dsts[i]];
-                    min_dst = i;
+        while (K > 0) {
+            vector<int> tempPath(n, n * 10001);
+            for (int i = 0; i < n; i++) {
+                if (i == src) continue;
+                
+                int min_path = path[src][i];
+                for (int j = 0; j < n; j++){
+                    if (j == src || j == i) continue;
+                    min_path = min(min_path, path[src][j] + path[j][i]);
                 }
+                tempPath[i] = min_path;
             }
-            
-            if (min_dst < 0) {
-                if (path[src][dst] < 10001) return path[src][dst];
-                return -1;
-            }
-            
-            if (dsts[min_dst] == dst) return path[src][dst];
-            
-            for (int i = 0; i < dsts.size(); i++) 
-                path[src][dsts[i]] = min(path[src][dsts[i]], path[src][dsts[min_dst]] + path[dsts[min_dst]][dsts[i]]);
-            
-            dsts.erase(dsts.begin() + min_dst);
+            path[src] = tempPath;
             K--; 
         }
-        return path[0][4];
         
         int result = path[src][dst];
-        if (K >0) {
-            for (int i = 0; i < n; i++) result = min(result, path[src][i] + path[i][dst]);
-        }
-        
-        if (result > (K + 1) * 10000) return -1;
+        if (result > (kk + 1) * 10000) return -1;
         return result; 
     }
 };
