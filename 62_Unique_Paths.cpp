7926383adf5df7da2dd4ce575949c19a71956398
zkxshg <zkxshg@126.com>
2020-08-01 04:46:30 +0800

    Update 62_Unique_Paths.cpp

--- 62_Unique_Paths.cpp
+++ 62_Unique_Paths.cpp
@@ -10,3 +10,14 @@ public:
         return multi;
     }
 };
+
+// DP
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        if (m == 1 || n == 1) return 1;
+        int result = 0;
+        result = uniquePaths(m - 1, n) + uniquePaths(m, n - 1);
+        return result;
+    }
+};
