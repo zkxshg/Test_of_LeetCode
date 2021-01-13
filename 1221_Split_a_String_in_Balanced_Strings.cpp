7f4e8ba55e858c80c4d5cc1f40279fc3a8a3bd40
zkxshg <zkxshg@126.com>
2021-01-14 05:05:11 +0800

    Create 1221_Split_a_String_in_Balanced_Strings.cpp

---
+++ 1221_Split_a_String_in_Balanced_Strings.cpp
@@ -0,0 +1,14 @@
+// greedy
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+        int sco = 0, res = 0;
+        for (char ch : s) {
+            if (ch == 'L') sco++;
+            else sco--;
+            
+            if (sco == 0) res++;
+        }
+        return res;
+    }
+};
