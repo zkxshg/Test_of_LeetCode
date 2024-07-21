f1aa7ff267714e2d480908d4ab24af0fc5d1caa4
zkxshg <zkxshg@126.com>
2024-07-21 08:31:05 +0800

    Create 1605_Find_Valid_Matrix_Given_Row_and_Column_Sums.cpp

---
+++ 1000-2000/1605_Find_Valid_Matrix_Given_Row_and_Column_Sums.cpp
@@ -0,0 +1,39 @@
+// https://leetcode.com/problems/find-valid-matrix-given-row-and-column-sums/?envType=daily-question&envId=2024-07-20
+// Math + Greedy
+
+class Solution {
+public:
+    vector<vector<int>> restoreMatrix(vector<int>& rowSum, vector<int>& colSum) {
+        int m = rowSum.size(), n = colSum.size();
+        
+        // Min-heaps of row and col sums
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> rowHeap, colHeap;
+        
+        // Initialize the heaps
+        for (int i = 0; i < m; ++i) rowHeap.push({rowSum[i], i});
+        for (int j = 0; j < n; ++j) colHeap.push({colSum[j], j});
+ 
+        // Initialize the result matrix
+        vector<vector<int>> res(m, vector<int>(n, 0));
+        
+        // Build the matrix
+        while (!rowHeap.empty() && !colHeap.empty()) {
+            auto [rowValue, rowIndex] = rowHeap.top();
+            auto [colValue, colIndex] = colHeap.top();
+            int fillAmount = min(rowValue, colValue);
+
+            res[rowIndex][colIndex] = fillAmount;
+
+            colHeap.pop();
+            rowHeap.pop();
+            if (rowValue > colValue) {
+                rowHeap.push({rowValue - fillAmount, rowIndex});
+            }
+            else if (rowValue < colValue) {
+                colHeap.push({colValue - fillAmount, colIndex});
+            } 
+        }
+        
+        return res;
+    }
+};
